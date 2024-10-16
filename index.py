1c1ca3bdab0b2fb1ac360aaf6cca1e2bd865a7a6
Noam <158506676+NotM3ry@users.noreply.github.com>
2024-10-16 08:26:24 +0200

    Delete index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-def nb_solutions(score):
-    if score == 0 or score == 3 or score == 6 or score == 5:
-        return 1
-    if score == 1 or score == 2 or score == 4:
-        return 0
-    return nb_solutions(score - 7) + nb_solutions(score - 5) + nb_solutions(score - 3)
-
-print(nb_solutions(11))
